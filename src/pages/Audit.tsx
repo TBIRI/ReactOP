03ab494b36a9aac4cedef52233df9d8138c53d12
TBIRI <theobieri07@gmail.com>
2026-02-10 09:57:17 +0100

    Updated Audit.tsx

--- src/pages/Audit.tsx
+++ src/pages/Audit.tsx
@@ -13,7 +13,20 @@ function Audit() {
   }, []);
 
   return (
-    <div style={{ margin: 0, height: '100vh', overflow: 'hidden' }}>
+    <div className="bg-black" style={{ margin: 0, height: '100vh', overflow: 'hidden' }}>
+      {/* Mobius logo - very blurred background */}
+      <div
+        className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[100vw] h-[100vh] min-w-[1200px] min-h-[1200px] lg:min-w-[1600px] lg:min-h-[1600px] pointer-events-none z-0"
+        style={{
+          backgroundImage: "url('/mobius_det.png')",
+          backgroundSize: 'contain',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          filter: 'blur(50px)',
+          opacity: 0.3
+        }}
+      ></div>
+
       <iframe
         data-tally-src="https://tally.so/r/Pd5RD1?transparentBackground=1"
         width="100%"
